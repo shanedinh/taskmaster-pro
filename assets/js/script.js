44e3f7c7ed5001b7866387d55dc47e850bca4eb1
Shane Dinh <shane.dinh5@gmail.com>
2022-05-25 19:42:05 -0500

    dynamically create buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,9 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-
-
+$(".list-group").on("click", "p", function() {
+  console.log("<p> was clicked");
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
